d0f77e3abc5038079a9dfafe7d96e767ab0c3c3e
Paolla Trevisan <153026926+PaollaTrevisan@users.noreply.github.com>
2024-03-14 13:29:21 -0300

    Create if.py

---
+++ if.py
@@ -0,0 +1,9 @@
+print("Olá, meu nome é Sabrina e vou te ajudar hoje.")
+nome = str(input("Por favor, me informe seu nome:"))
+print("Certo!",nome)
+idade = int(input("Preciso que me informe sua idade"))
+if idade >= 18: 
+    print("Sua idade confere nos termos de uso do site")
+    print("Lembre-se: Apostas são arriscadas, esteja conciente da quantia que está arriscando.")
+else:
+    print("Ops! Você precisa ter maioridade para utilizar o site de apostas")
